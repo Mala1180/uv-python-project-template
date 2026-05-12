fbb1f9cc1c1eb1acd9f74ae3ceaaba70dd1e0150
Mattia Matteini <matteinimattia@gmail.com>
2026-05-12 15:27:00 +0200

    feat: setup project with uv and pypi release

--- release.config.mjs
+++ release.config.mjs
@@ -2,12 +2,11 @@ let dryRun = (process.env.RELEASE_DRY_RUN || "false").toLowerCase() === "true";
 let testPypi = (process.env.RELEASE_TEST_PYPI || "false").toLowerCase() === "true";
 const pypiToken = process.env.PYPI_TOKEN;
 
-let prepareCmd = "poetry version -- \${nextRelease.version}" + ` && poetry config pypi-token.pypi ${pypiToken}`;
-let publishCmd = `poetry publish --build`;
+let prepareCmd = "uv version \${nextRelease.version} && uv build";
+let publishCmd = `uv publish --token ${pypiToken}`;
 
 if (testPypi) {
-    publishCmd += ` --repository testpypi`;
-    prepareCmd = prepareCmd.replace("pypi-token.pypi", "pypi-token.testpypi");
+    publishCmd += ` --index testpypi`;
 }
 
 if (dryRun) {
